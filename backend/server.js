e09593d22bbc6d722d6c84fa39656018b379deb8
Gustaf Ekström <gusek93@gmail.com>
2024-07-30 17:14:52 +0200

    Styling and fixes. Fetches answers

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 4000;
 async function makeRequest(prompt) {
   try {
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "prompt" }],
+      messages: [{ role: "user", content: prompt}],
       model: "gpt-3.5-turbo",
     });
     console.log(completion.choices[0]);
--- frontend-openai/src/App.tsx
+++ frontend-openai/src/App.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import axios from "axios";
 
+type aiMessage = {
+  prompt: string;
+  answer: string[];
+};
+
 function App() {
-  const [responses, setResponses] = useState<string[]>([]);
+  const [responses, setResponses] = useState<aiMessage[]>([]);
   const [currentPrompt, setCurrentPrompt] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   async function makePrompt(prompt: string) {
     console.log(prompt);
@@ -12,33 +18,68 @@ function App() {
         prompt: prompt,
       },
     });
-    console.log(response.data);
-    setResponses([...responses, response.data.answer]);
+    //console.log(response.data.answer.split("\n"));
+    setResponses([
+      ...responses,
+      { prompt: prompt, answer: response.data.answer.split("\n") },
+    ]);
+  }
+
+  function handleSubmit() {
+    if (currentPrompt === "") {
+      setErrorMessage("Please enter a prompt.");
+      setTimeout(() => setErrorMessage(""), 2000);
+      return;
+    }
+    makePrompt(currentPrompt);
+    setCurrentPrompt("");
   }
 
   return (
     <div className="flex flex-col gap-5 items-center border-2  w-[600px] m-auto py-10 my-10 rounded-xl bg-stone-100">
       <h1 className="font-bold text-3xl text-stone-950">AI Chatbot</h1>
-      <div className=" flex justify-center flex-col items-center gap-5">
-        <div className="flex flex-col gap-5 overflow-y-auto h-[500px] w-[500px] rounded-lg bg-white font-semibold text-stone-950 items-start p-10 text-sm shadow-stone-950 shadow-sm">
-          <h2 className="text-xl border-b-2 w-full border-stone-950">
-            Responses from AI bot
-          </h2>
+      <div className=" flex justify-center flex-col items-center gap-2">
+        <div className="flex flex-col gap-5 overflow-y-auto h-[500px] w-[500px] rounded-lg bg-white font-semibold text-stone-950 items-start p-5 text-sm shadow-stone-950 shadow-sm">
           {responses.length !== 0 ? (
             responses.map((response) => (
-              <p key={responses.indexOf(response)}>{response}</p>
+              <div
+                className="shadow-inner flex flex-col gap-2 "
+                key={responses.indexOf(response)}
+              >
+                <p className="bg-stone-200 p-2 inline-block rounded-md shadow-sm shadow-stone-950">
+                  {response.prompt}
+                </p>
+                <div className=" p-2 inline-block rounded-md shadow-sm shadow-stone-950">
+                  {response.answer.map((item: string) => (
+                    <p className="py-1" key={item}>
+                      {item}
+                    </p>
+                  ))}
+                </div>
+              </div>
             ))
           ) : (
-            <div className=" flex flex-col items-start">
-              <p>Welcome!</p>
-              <p>What can I help you with today?</p>
+            <div className=" flex flex-col items-start gap-2">
+              <p className="bg-stone-200 shadow-sm shadow-stone-950 rounded-lg p-3">
+                Welcome!
+              </p>
+              <p className="bg-stone-200 shadow-sm shadow-stone-950 rounded-lg p-3">
+                What can I help you with today?
+              </p>
             </div>
+          )}
+        </div>
+        <div className="h-6 w-full">
+          {errorMessage && (
+            <span className="text-red-500 font-semibold items-start flex justify-start px-8">
+              {errorMessage}
+            </span>
           )}
         </div>
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            makePrompt(currentPrompt);
+            handleSubmit();
           }}
           className="flex px-5 gap-10"
         >
@@ -46,10 +87,11 @@ function App() {
             className="p-3 text-black rounded-lg font-semibold w-96 shadow-sm shadow-stone-950"
             type="text"
             placeholder="Enter your prompt"
+            value={currentPrompt}
             onChange={(e) => setCurrentPrompt(e.target.value)}
           />
           <button
-            className="bg-white shadow-sm shadow-stone-950 text-stone-950 font-semibold rounded-lg w-20 hover:shadow-blue-700 hover:bg-stone-50"
+            className="bg-white shadow-sm shadow-stone-950 text-stone-950 font-semibold rounded-lg w-20 hover:bg-stone-950 hover:text-stone-100"
             type="submit"
           >
             Send
